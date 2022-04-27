2330ebbf2d69f13ba3488cb950e1ac11fc95fb51
Igor Massonetto <igormassonetto@gmail.com>
2022-04-26 21:15:25 -0300

    first commit

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const __dirname = path.resolve(path.dirname(''))
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
 app.set("view engine", "ejs") 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded());
